d8151d1726add6a0a614e54870f8a3f8e78e1efb
mohamed <mohammedbelfares@gmail.com>
2023-06-13 17:55:26 +0000

    modifiaction

--- public/js/app.js
+++ public/js/app.js
@@ -4,6 +4,7 @@ let equal = document.querySelector("#equal")
 let derniereChar ; 
 let previousChar;
 let sreenlenght;
+let tab = ["+","-","*","/"]
 
 
 
@@ -18,9 +19,12 @@ let sreenlenght;
 //////// qu'on clicll sur un button en affiche dans le screen
 function screen(number) {
     input.value +=number;
+    corect()
     sreenlenght = input.value.length;
     derniereChar = number;
+    
     previous()
+    
 }
 
 
@@ -40,7 +44,45 @@ equal.addEventListener("click",()=>{
 
 function previous() {
     previousChar = input.value.substring(sreenlenght-2,sreenlenght-1);
+    calSynat();
 
-    console.log(previousChar);
-    console.log(derniereChar);
 }
+
+
+////////////// function de vrai syntaxhe ///////////////
+
+function calSynat() {
+   
+
+    if(tab.includes(derniereChar) && sreenlenght==1){
+        removechar()
+    }
+    if (tab.includes(derniereChar) && tab.includes(previousChar)) {
+        if(derniereChar == previousChar){
+             removechar();
+        }
+        else{
+           input.value =  input.value.slice(0,sreenlenght-2)+input.value.slice(input.value.length-1)
+           console.log(input.value);
+        }
+       
+    }
+   
+}
+
+
+//////// function remove last char //////////////
+
+function removechar() {
+    input.value = input.value.substring(0,input.value.length-1)
+}
+
+/////////// 0 //////////
+function corect() {
+    
+   if (input.value.charAt(0)==0) {
+       input.value= input.value.substring(1,input.value.length-1);
+        console.log(input.value);
+   }
+}
+
